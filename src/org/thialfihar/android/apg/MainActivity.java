2e1aad0f81c1511c741b391b4d8254f92ba56271
Thialfihar <thialfihar@gmail.com>
2010-05-04 23:19:39 +0000

    reset changelog for 0.9.4

--- src/org/thialfihar/android/apg/MainActivity.java
+++ src/org/thialfihar/android/apg/MainActivity.java
@@ -221,10 +221,6 @@ public class MainActivity extends BaseActivity {
                 SpannableString info =
                         new SpannableString("Read the warnings!\n\n" +
                                             "Changes:\n" +
-                                            "* option to delete file after encryption/decryption\n" +
-                                            "* fixed several crashes\n" +
-                                            "* can encrypt large files\n" +
-                                            "* better progress bar calculation\n" +
                                             "\n" +
                                             "WARNING: be careful editing your existing keys, as they " +
                                             "WILL be stripped of certificates right now.\n" +
