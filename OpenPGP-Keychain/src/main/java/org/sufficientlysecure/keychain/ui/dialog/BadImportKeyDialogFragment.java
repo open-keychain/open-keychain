87af73977332b698c94d1ff8b3863cddc51840d5
Nikhil Peter Raj <nikhilpeter93@gmail.com>
2014-02-27 18:09:47 +0530

    Added GPL headers

--- OpenPGP-Keychain/src/main/java/org/sufficientlysecure/keychain/ui/dialog/BadImportKeyDialogFragment.java
+++ OpenPGP-Keychain/src/main/java/org/sufficientlysecure/keychain/ui/dialog/BadImportKeyDialogFragment.java
@@ -1,5 +1,20 @@
 package org.sufficientlysecure.keychain.ui.dialog;
-
+/*
+ * Copyright (C) 2012-2013 Dominik Schürmann <dominik@dominikschuermann.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -10,10 +25,6 @@ import android.support.v4.app.FragmentActivity;
 
 import org.sufficientlysecure.keychain.R;
 
-
-/**
- * Created by Haven on 26/2/14.
- */
 public class BadImportKeyDialogFragment extends DialogFragment {
     private static final String ARG_BAD_IMPORT = "bad_import";
 
