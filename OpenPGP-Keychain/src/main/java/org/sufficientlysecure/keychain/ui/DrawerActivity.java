16116ba72572fc85fe00ec422248818da516506a
Vincent Breitmoser <valodim@mugenguild.com>
2014-03-08 13:42:50 +0100

    ditch old key list activities

--- OpenPGP-Keychain/src/main/java/org/sufficientlysecure/keychain/ui/DrawerActivity.java
+++ OpenPGP-Keychain/src/main/java/org/sufficientlysecure/keychain/ui/DrawerActivity.java
@@ -52,7 +52,7 @@ public class DrawerActivity extends ActionBarActivity {
 
     private static Class[] mItemsClass = new Class[] { KeyListActivity.class,
             EncryptActivity.class, DecryptActivity.class, ImportKeysActivity.class,
-            KeyListSecretActivity.class, RegisteredAppsListActivity.class };
+            RegisteredAppsListActivity.class };
     private Class mSelectedItem;
 
     private static final int MENU_ID_PREFERENCE = 222;
@@ -72,7 +72,6 @@ public class DrawerActivity extends ActionBarActivity {
                 new NavItem("fa-lock", getString(R.string.nav_encrypt)),
                 new NavItem("fa-unlock", getString(R.string.nav_decrypt)),
                 new NavItem("fa-download", getString(R.string.nav_import)),
-                new NavItem("fa-key", getString(R.string.nav_secret_keys)),
                 new NavItem("fa-android", getString(R.string.nav_apps)) };
 
         mDrawerList.setAdapter(new NavigationDrawerAdapter(this, R.layout.drawer_list_item,
