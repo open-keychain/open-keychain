b12392594f54cac1ef8812c80decc315f70866db
uberspot <onexemailx@gmail.com>
2014-03-13 23:52:21 +0200

    Make email address less restrictive and add icon to show when it's correct

--- OpenPGP-Keychain/src/main/java/org/sufficientlysecure/keychain/ui/widget/UserIdEditor.java
+++ OpenPGP-Keychain/src/main/java/org/sufficientlysecure/keychain/ui/widget/UserIdEditor.java
@@ -17,6 +17,8 @@
 package org.sufficientlysecure.keychain.ui.widget;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.Patterns;
 import android.view.View;
@@ -102,6 +104,31 @@ public class UserIdEditor extends LinearLayout implements Editor, OnClickListene
                         (this.getContext(), android.R.layout.simple_dropdown_item_1line,
                                 ContactHelper.getMailAccounts(getContext())
                         ));
+        mEmail.addTextChangedListener(new TextWatcher(){
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) { }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) { }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String email = editable.toString();
+                if (email.length() > 0) {
+                    Matcher emailMatcher = Patterns.EMAIL_ADDRESS.matcher(email);
+                    if (emailMatcher.matches()) {
+                        mEmail.setCompoundDrawablesWithIntrinsicBounds(0, 0,
+                                    android.R.drawable.presence_online, 0);
+                    } else {
+                        mEmail.setCompoundDrawablesWithIntrinsicBounds(0, 0,
+                                    android.R.drawable.presence_offline, 0);
+                    }
+                } else {
+                    // remove drawable if email is empty
+                    mEmail.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                }
+            }
+        });
 
         super.onFinishInflate();
     }
@@ -129,18 +156,10 @@ public class UserIdEditor extends LinearLayout implements Editor, OnClickListene
         }
     }
 
-    public String getValue() throws NoNameException, NoEmailException, InvalidEmailException {
+    public String getValue() throws NoNameException, NoEmailException {
         String name = ("" + mName.getText()).trim();
         String email = ("" + mEmail.getText()).trim();
         String comment = ("" + mComment.getText()).trim();
-
-        if (email.length() > 0) {
-            Matcher emailMatcher = Patterns.EMAIL_ADDRESS.matcher(email);
-            if (!emailMatcher.matches()) {
-                throw new InvalidEmailException(getContext().getString(R.string.error_invalid_email,
-                        email));
-            }
-        }
 
         String userId = name;
         if (comment.length() > 0) {
