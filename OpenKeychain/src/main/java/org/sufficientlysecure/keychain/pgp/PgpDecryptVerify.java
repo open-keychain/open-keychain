eb9d8c5c3f42fb020bdb598a3895bf5f0b90ff98
Dominik Schürmann <dominik@dominikschuermann.de>
2014-04-16 00:38:03 +0200

    comments

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/pgp/PgpDecryptVerify.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/pgp/PgpDecryptVerify.java
@@ -439,7 +439,7 @@ public class PgpDecryptVerify {
 
                 signatureResultBuilder.signatureAvailable(true);
                 signatureResultBuilder.knownKey(true);
-                // TODO: uses the first pubkey for information
+                // TODO: uses the first pubkey for information, is this the master key?
                 signatureResultBuilder.userId(PgpKeyHelper.getMainUserId(publicKeyRing.getPublicKey()));
                 signatureResultBuilder.keyId(publicKeyRing.getPublicKey().getKeyID());
 
@@ -640,7 +640,7 @@ public class PgpDecryptVerify {
 
             signatureResultBuilder.signatureAvailable(true);
             signatureResultBuilder.knownKey(true);
-            // TODO: uses the first pubkey for information
+            // TODO: uses the first pubkey for information, is this the master key?
             signatureResultBuilder.userId(PgpKeyHelper.getMainUserId(publicKeyRing.getPublicKey()));
             signatureResultBuilder.keyId(publicKeyRing.getPublicKey().getKeyID());
 
