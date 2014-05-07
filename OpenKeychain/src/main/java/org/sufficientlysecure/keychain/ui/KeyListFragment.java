65274f6c1678c55d321e513c3db45974c68470e4
Vincent Breitmoser <valodim@mugenguild.com>
2014-05-07 03:07:24 +0200

    ui: purplize searchtext in keylist

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/ui/KeyListFragment.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/ui/KeyListFragment.java
@@ -381,6 +381,9 @@ public class KeyListFragment extends Fragment
         // Execute this when searching
         mSearchView.setOnQueryTextListener(this);
 
+        View searchPlate = mSearchView.findViewById(android.support.v7.appcompat.R.id.search_plate);
+        searchPlate.setBackgroundResource(R.drawable.keychaintheme_searchview_holo_light);
+
         // Erase search result without focus
         MenuItemCompat.setOnActionExpandListener(searchItem, new MenuItemCompat.OnActionExpandListener() {
             @Override
