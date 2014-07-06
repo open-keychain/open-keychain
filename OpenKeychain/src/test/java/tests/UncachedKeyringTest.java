e906fe53870660bd7231e45acce2a7c525edeb91
Art O Cathain <art.home@gmail.com>
2014-07-06 17:35:07 +0100

    add the GPG version

--- OpenKeychain/src/test/java/tests/UncachedKeyringTest.java
+++ OpenKeychain/src/test/java/tests/UncachedKeyringTest.java
@@ -10,6 +10,7 @@ import org.sufficientlysecure.keychain.testsupport.KeyringBuilder;
 import org.sufficientlysecure.keychain.testsupport.TestDataUtil;
 
 import java.io.*;
+import java.util.Collections;
 
 @RunWith(RobolectricTestRunner.class)
 @org.robolectric.annotation.Config(emulateSdk = 18) // Robolectric doesn't yet support 19
@@ -19,10 +20,18 @@ public class UncachedKeyringTest {
     public void testVerifySuccess() throws Exception {
         UncachedKeyRing expectedKeyRing = KeyringBuilder.ring2();
         UncachedKeyRing inputKeyRing = KeyringBuilder.ring1();
-        // Uncomment to dump the encoded key for manual inspection
+//        Uncomment to dump the encoded key for manual inspection
 //        inputKeyRing.getPublicKey().getPublicKey().encode(new FileOutputStream(new File("/tmp/key-encoded")));
         new UncachedKeyringTestingHelper().doTestCanonicalize(inputKeyRing, expectedKeyRing);
     }
+
+    @Test
+    public void testVerifyFromGpg() throws Exception {
+        byte[] data = TestDataUtil.readAllFully(Collections.singleton( "/public-key-canonicalize.blob"));
+        UncachedKeyRing inputKeyRing = UncachedKeyRing.decodeFromData(data);
+        new UncachedKeyringTestingHelper().doTestCanonicalize(inputKeyRing, KeyringBuilder.ring2());
+    }
+
 
     /**
      * Just testing my own test code. Should really be using a library for this.
