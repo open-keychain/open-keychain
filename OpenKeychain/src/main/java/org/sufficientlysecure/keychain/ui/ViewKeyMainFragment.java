23689da56db1c4f5e53d6e028a619f357a73586c
Dominik Schürmann <dominik@dominikschuermann.de>
2014-07-28 12:24:40 +0200

    Use verified icon for certify action

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/ui/ViewKeyMainFragment.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/ui/ViewKeyMainFragment.java
@@ -19,6 +19,7 @@ package org.sufficientlysecure.keychain.ui;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
@@ -27,6 +28,7 @@ import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.devspark.appmsg.AppMsg;
@@ -52,6 +54,8 @@ public class ViewKeyMainFragment extends LoaderFragment implements
     private View mActionEditDivider;
     private View mActionEncrypt;
     private View mActionCertify;
+    private View mActionCertifyText;
+    private ImageView mActionCertifyImage;
     private View mActionCertifyDivider;
 
     private ListView mUserIds;
@@ -76,6 +80,11 @@ public class ViewKeyMainFragment extends LoaderFragment implements
         mActionEditDivider = view.findViewById(R.id.view_key_action_edit_divider);
         mActionEncrypt = view.findViewById(R.id.view_key_action_encrypt);
         mActionCertify = view.findViewById(R.id.view_key_action_certify);
+        mActionCertifyText = view.findViewById(R.id.view_key_action_certify_text);
+        mActionCertifyImage = (ImageView) view.findViewById(R.id.view_key_action_certify_image);
+        // make certify image gray, like action icons
+        mActionCertifyImage.setColorFilter(getResources().getColor(R.color.tertiary_text_light),
+                PorterDuff.Mode.SRC_IN);
         mActionCertifyDivider = view.findViewById(R.id.view_key_action_certify_divider);
 
         return root;
@@ -182,6 +191,7 @@ public class ViewKeyMainFragment extends LoaderFragment implements
                     if (data.getInt(INDEX_UNIFIED_IS_REVOKED) != 0) {
                         mActionEdit.setEnabled(false);
                         mActionCertify.setEnabled(false);
+                        mActionCertifyText.setEnabled(false);
                         mActionEncrypt.setEnabled(false);
                     } else {
                         mActionEdit.setEnabled(true);
@@ -189,9 +199,11 @@ public class ViewKeyMainFragment extends LoaderFragment implements
                         Date expiryDate = new Date(data.getLong(INDEX_UNIFIED_EXPIRY) * 1000);
                         if (!data.isNull(INDEX_UNIFIED_EXPIRY) && expiryDate.before(new Date())) {
                             mActionCertify.setEnabled(false);
+                            mActionCertifyText.setEnabled(false);
                             mActionEncrypt.setEnabled(false);
                         } else {
                             mActionCertify.setEnabled(true);
+                            mActionCertifyText.setEnabled(true);
                             mActionEncrypt.setEnabled(true);
                         }
                     }
@@ -252,8 +264,6 @@ public class ViewKeyMainFragment extends LoaderFragment implements
     private void editKey(Uri dataUri) {
         Intent editIntent = new Intent(getActivity(), EditKeyActivity.class);
         editIntent.setData(KeychainContract.KeyRingData.buildSecretKeyRingUri(dataUri));
-//        editIntent.setAction(EditKeyActivity.ACTION_EDIT_KEY);
-//        startActivityForResult(editIntent, 0);
         startActivity(editIntent);
     }
 
