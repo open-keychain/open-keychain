9fab740aadb76883105b61451c047fc1d4b27f1f
mar-v-in <github@rvin.mooo.com>
2014-08-14 12:22:44 +0200

    add HAS_CERTIFY (not sure why it's missing)

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/provider/KeychainContract.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/provider/KeychainContract.java
@@ -111,6 +111,7 @@ public class KeychainContract {
         public static final String HAS_ANY_SECRET = "has_any_secret";
         public static final String HAS_ENCRYPT = "has_encrypt";
         public static final String HAS_SIGN = "has_sign";
+        public static final String HAS_CERTIFY = "has_certify";
         public static final String PUBKEY_DATA = "pubkey_data";
         public static final String PRIVKEY_DATA = "privkey_data";
 
