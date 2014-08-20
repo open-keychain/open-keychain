dd1b27b28f7c84cc10a4dd632b2175d9171f6c72
Vincent Breitmoser <valodim@mugenguild.com>
2014-08-20 21:52:54 +0200

    consolidate: small ui things

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/provider/ProviderHelper.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/provider/ProviderHelper.java
@@ -832,6 +832,8 @@ public class ProviderHelper {
         log(LogLevel.START, LogType.MSG_CON);
         mIndent += 1;
 
+        progress.setProgress(R.string.progress_con_saving, 0, 100);
+
         try {
 
             log(LogLevel.DEBUG, LogType.MSG_CON_SAVE_SECRET);
@@ -890,6 +892,8 @@ public class ProviderHelper {
         } finally {
             mIndent -= 1;
         }
+
+        progress.setProgress(R.string.progress_con_saving, 3, 100);
 
         // 1b. fetch all public keyrings into a cache file
         try {
