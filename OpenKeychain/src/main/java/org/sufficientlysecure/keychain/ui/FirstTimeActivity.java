17e45d94a827e97b1f276c483035f2903423740d
Dominik Schürmann <dominik@dominikschuermann.de>
2014-10-02 11:23:43 +0200

    Add workaround for bug #912

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/ui/FirstTimeActivity.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/ui/FirstTimeActivity.java
@@ -20,6 +20,7 @@ package org.sufficientlysecure.keychain.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 
@@ -97,5 +98,11 @@ public class FirstTimeActivity extends ActionBarActivity {
         }
         startActivityForResult(intent, 0);
         finish();
+    }
+
+    // workaround for https://code.google.com/p/android/issues/detail?id=61394
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return keyCode == KeyEvent.KEYCODE_MENU || super.onKeyDown(keyCode, event);
     }
 }
