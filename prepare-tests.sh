8f5295f2d13e895490813c0ced19cfd738e591ac
Dominik Schürmann <dominik@dominikschuermann.de>
2014-12-04 19:42:30 +0100

    Update to com.novoda:gradle-android-test-plugin:0.10.0, no snapshot version anymore, jacoco disabled for now

--- prepare-tests.sh
+++ prepare-tests.sh
@@ -10,16 +10,16 @@ if ! java -version 2>&1 | grep OpenJDK; then
     return
 fi
 
-tmpdir="$(mktemp -d)"
-(
-  cd "$tmpdir";
-  git clone https://github.com/nenick/gradle-android-test-plugin.git
-  cd gradle-android-test-plugin
-  echo "rootProject.name = 'gradle-android-test-plugin-parent'" > settings.gradle
-  echo "include ':gradle-android-test-plugin'" >> settings.gradle
-  ./gradlew :gradle-android-test-plugin:install
-)
-rm -rf "$tmpdir"
+#tmpdir="$(mktemp -d)"
+#(
+#  cd "$tmpdir";
+#  git clone https://github.com/nenick/gradle-android-test-plugin.git
+#  cd gradle-android-test-plugin
+#  echo "rootProject.name = 'gradle-android-test-plugin-parent'" > settings.gradle
+#  echo "include ':gradle-android-test-plugin'" >> settings.gradle
+#  ./gradlew :gradle-android-test-plugin:install
+#)
+#rm -rf "$tmpdir"
 
 echo -n "ok, adding tests to include list.. "
 if grep OpenKeychain-Test settings.gradle >/dev/null ; then
