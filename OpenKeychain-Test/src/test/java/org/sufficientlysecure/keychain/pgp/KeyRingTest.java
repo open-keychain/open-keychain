484c3092eecbf775f355879587c2bea7612edac7
Dominik Schürmann <dominik@dominikschuermann.de>
2015-03-16 13:54:12 +0100

    object oriented split user id

---
+++ OpenKeychain-Test/src/test/java/org/sufficientlysecure/keychain/pgp/KeyRingTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2015 Dominik Schürmann <dominik@dominikschuermann.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.sufficientlysecure.keychain.pgp;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+@org.robolectric.annotation.Config(emulateSdk = 18) // Robolectric doesn't yet support 19
+public class KeyRingTest {
+
+    @Test
+    public void splitCompleteUserIdShouldReturnAll3Components() throws Exception {
+        KeyRing.UserId info = KeyRing.splitUserId("Max Mustermann (this is a comment) <max@example.com>");
+        Assert.assertEquals("Max Mustermann", info.name);
+        Assert.assertEquals("this is a comment", info.comment);
+        Assert.assertEquals("max@example.com", info.email);
+    }
+
+    @Test
+    public void splitUserIdWithAllButCommentShouldReturnNameAndEmail() throws Exception {
+        KeyRing.UserId info = KeyRing.splitUserId("Max Mustermann <max@example.com>");
+        Assert.assertEquals("Max Mustermann", info.name);
+        Assert.assertNull(info.comment);
+        Assert.assertEquals("max@example.com", info.email);
+    }
+
+    @Test
+    public void splitUserIdWithAllButEmailShouldReturnNameAndComment() throws Exception {
+        KeyRing.UserId info = KeyRing.splitUserId("Max Mustermann (this is a comment)");
+        Assert.assertEquals(info.name, "Max Mustermann");
+        Assert.assertEquals(info.comment, "this is a comment");
+        Assert.assertNull(info.email);
+    }
+
+}
