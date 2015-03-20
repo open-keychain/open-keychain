50913d0ff1f29d4d32c36b1924b20fc8ef3a76bc
Kent <itaqmail@gmail.com>
2015-03-20 12:38:16 +0800

    Updated GNU header

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/util/NfcHelper.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/util/NfcHelper.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2014 Vincent Breitmoser <v.breitmoser@mugenguild.com>
+ * Copyright (C) 2013-2014 Dominik Schürmann <dominik@dominikschuermann.de>
+ * Copyright (C) 2015 Kent Nguyen <kentnguyen@moneylover.me>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
