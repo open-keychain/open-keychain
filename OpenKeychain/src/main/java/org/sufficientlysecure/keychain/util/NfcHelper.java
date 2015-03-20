e0a5f1a0d44d49afffb1c5df1420431f39586c55
Kent <itaqmail@gmail.com>
2015-03-20 12:24:06 +0800

    Removed create and added GPLv3 header

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/util/NfcHelper.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/util/NfcHelper.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2014 Vincent Breitmoser <v.breitmoser@mugenguild.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.sufficientlysecure.keychain.util;
 
 import android.annotation.TargetApi;
@@ -24,10 +41,6 @@ import java.lang.ref.WeakReference;
 
 /**
  * This class contains NFC functionality that can be shared across Fragments or Activities.
- * <p/>
- * Created on Mar 20, 2015.
- *
- * @author Kent
  */
 
 public class NfcHelper {
