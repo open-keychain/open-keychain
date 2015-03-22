3a53557cd492bd97f8bcc7534f6c55d597a0d927
hashbash <sharad.gupta@students.iiit.ac.in>
2015-03-22 10:21:07 +0530

    Clear passphase cache

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/service/PassphraseCacheService.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/service/PassphraseCacheService.java
@@ -93,7 +93,7 @@ public class PassphraseCacheService extends Service {
     public static final String EXTRA_MESSENGER = "messenger";
     public static final String EXTRA_USER_ID = "user_id";
 
-    private static final int REQUEST_ID = 0;
+
     private static final long DEFAULT_TTL = 15;
 
     private static final int NOTIFICATION_ID = 1;
@@ -314,7 +314,8 @@ public class PassphraseCacheService extends Service {
     private static PendingIntent buildIntent(Context context, long keyId) {
         Intent intent = new Intent(BROADCAST_ACTION_PASSPHRASE_CACHE_SERVICE);
         intent.putExtra(EXTRA_KEY_ID, keyId);
-        return PendingIntent.getBroadcast(context, REQUEST_ID, intent,
+        Long _id = System.currentTimeMillis();
+        return PendingIntent.getBroadcast(context, _id.intValue(), intent,
                 PendingIntent.FLAG_CANCEL_CURRENT);
     }
 
