0a7abd0c1ff8ca3c25322a15a5b801f81528cb9e
Adithya Abraham Philip <adithyaphilip@gmail.com>
2015-06-17 11:39:22 +0530

    support for new minidns lib

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/service/KeychainService.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/service/KeychainService.java
@@ -73,14 +73,14 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import de.measite.minidns.Client;
+import de.measite.minidns.DNSCache;
+import de.measite.minidns.DNSClient;
 import de.measite.minidns.DNSMessage;
 import de.measite.minidns.Question;
 import de.measite.minidns.Record;
 import de.measite.minidns.record.Data;
 import de.measite.minidns.record.TXT;
 import org.sufficientlysecure.keychain.util.ParcelableProxy;
-import org.sufficientlysecure.keychain.util.Preferences;
 
 /**
  * This Service contains all important long lasting operations for OpenKeychain. It receives Intents with
@@ -254,7 +254,8 @@ public class KeychainService extends Service implements Progressable {
 
                             String domain = prover.dnsTxtCheckRequired();
                             if (domain != null) {
-                                DNSMessage dnsQuery = new Client().query(new Question(domain, Record.TYPE.TXT));
+                                DNSMessage dnsQuery = new DNSClient((DNSCache) null).query(new Question(domain,
+                                        Record.TYPE.TXT));
                                 if (dnsQuery == null) {
                                     sendProofError(prover.getLog(), getString(R.string.keybase_dns_query_failure));
                                     return;
@@ -329,7 +330,8 @@ public class KeychainService extends Service implements Progressable {
                         boolean isSecret = data.getBoolean(DELETE_IS_SECRET);
 
                         // Operation
-                        DeleteOperation op = new DeleteOperation(KeychainService.this, providerHelper, KeychainService.this);
+                        DeleteOperation op = new DeleteOperation(KeychainService.this, providerHelper,
+                                KeychainService.this);
                         DeleteResult result = op.execute(masterKeyIds, isSecret);
 
                         // Result
@@ -434,11 +436,12 @@ public class KeychainService extends Service implements Progressable {
                             HkpKeyserver server = new HkpKeyserver(keyServer);
 
                             CanonicalizedPublicKeyRing keyring = providerHelper.getCanonicalizedPublicKeyRing(dataUri);
-                            ImportExportOperation importExportOperation = new ImportExportOperation(KeychainService.this,
+                            ImportExportOperation importExportOperation = new ImportExportOperation(KeychainService
+                                    .this,
                                     providerHelper, KeychainService.this);
 
                             ExportResult uploadResult = importExportOperation.uploadKeyRingToServer(server, keyring,
-                                        getProxyFromBundle(data));
+                                    getProxyFromBundle(data));
                             if (uploadResult.getResult() != ExportResult.RESULT_OK) {
                                 throw new PgpGeneralException("Unable to export key to selected server");
                             }
