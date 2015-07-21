c2bac2058d4a958388955761383cde88d6614170
Daniel Ramos <hiperzone@gmail.com>
2015-07-22 00:57:47 +0100

    -added Mifare Ultra Light class. -added nfc tag tech interface file

---
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/nfc/NfcTagTechnology.java
@@ -0,0 +1,21 @@
+package org.sufficientlysecure.keychain.nfc;
+
+
+import java.io.IOException;
+
+/**
+ * Nfc Tag Technology interface.
+ */
+public interface NfcTagTechnology {
+    void connect() throws IOException;
+
+    void upload(byte[] data) throws NfcDispatcher.CardException;
+
+    byte[] read() throws NfcDispatcher.CardException;
+
+    boolean verify(byte[] original, byte[] fromNFC) throws NfcDispatcher.CardException;
+
+    void close() throws NfcDispatcher.CardException;
+
+    boolean isConnected();
+}
