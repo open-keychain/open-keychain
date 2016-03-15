cab3fa7874f90f9aa8f5d3b9244e78ac4dd96638
Advaita <adipandya@gmail.com>
2016-03-15 23:07:27 +0530

    Final commit
    All changes included
    
    Improvements
    
    -Reformatted code wherever required
    -Only active wifi connections trigger events
    -Improve sync reliability
    -Removed extra permission
    
    Minor Changes
    
    -Refactored 2 variables for easier readbility
    -Wifi-Only-Sync is now enabled by default

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/ui/SettingsActivity.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/ui/SettingsActivity.java
@@ -19,8 +19,6 @@
 package org.sufficientlysecure.keychain.ui;
 
 
-import java.util.List;
-
 import android.Manifest;
 import android.accounts.Account;
 import android.accounts.AccountManager;
@@ -58,6 +56,8 @@ import org.sufficientlysecure.keychain.ui.util.ThemeChanger;
 import org.sufficientlysecure.keychain.util.Log;
 import org.sufficientlysecure.keychain.util.Preferences;
 import org.sufficientlysecure.keychain.util.orbot.OrbotHelper;
+
+import java.util.List;
 
 public class SettingsActivity extends AppCompatPreferenceActivity {
 
@@ -405,7 +405,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     }
 
     /**
-     * This fragment shows the keyserver/contacts sync preferences
+     * This fragment shows the keyserver/wifi-only-sync/contacts sync preferences
      */
     public static class SyncPrefsFragment extends PresetPreferenceFragment {
 
