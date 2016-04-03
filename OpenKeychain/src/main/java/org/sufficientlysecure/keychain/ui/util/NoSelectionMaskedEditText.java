40a3b1236feb3857220a3d19971d4b9d164b1d90
Alex Fong <alexfongg@gmail.com>
2016-04-03 12:47:44 +0800

    Prevent crashing caused by MaskedEditText
    Crashing is caused by mass deletion of text. Two fixes are introduced to prevent this.
    
    1) Prevent mass delete when backspace button is held
    SuccessTextWatcher is modified to watch for mass delete, and to emulate a single delete if detected.
    
    2) Prevent mass selection when double tapping
    OnTouch method of MaskedEditText is overridden, limiting selection size to 0.
    
    Note that fix 2) is required as allowing mass selection but not allowing mass delete does not make sense in a UX perspective.

---
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/ui/util/NoSelectionMaskedEditText.java
@@ -0,0 +1,69 @@
+package org.sufficientlysecure.keychain.ui.util;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.SystemClock;
+import android.text.InputFilter;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+
+import com.github.pinball83.maskededittext.MaskedEditText;
+
+import java.util.ListIterator;
+
+public class NoSelectionMaskedEditText extends MaskedEditText {
+    private long mLastTouchTime = SystemClock.uptimeMillis();
+
+    public NoSelectionMaskedEditText(Context context) {
+        super(context);
+    }
+
+    public NoSelectionMaskedEditText(Context context, String mask, String notMaskedSymbol) {
+        super(context, mask, notMaskedSymbol);
+    }
+
+    public NoSelectionMaskedEditText(Context context, String mask, String notMaskedSymbol, Drawable maskIcon) {
+        super(context, mask, notMaskedSymbol, maskIcon);
+    }
+
+    public NoSelectionMaskedEditText(Context context, String mask, String notMaskedSymbol, Drawable maskIcon, MaskIconCallback maskIconCallback) {
+        super(context, mask, notMaskedSymbol, maskIcon, maskIconCallback);
+    }
+
+    public NoSelectionMaskedEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NoSelectionMaskedEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int prevSelectionPosition = this.getSelectionStart();
+
+        boolean result = super.onTouchEvent(event);
+        if(isDoubleTap(event)) {
+            this.setSelection(prevSelectionPosition);
+        }
+
+        mLastTouchTime = SystemClock.uptimeMillis();
+        return result;
+    }
+
+    private boolean isDoubleTap(MotionEvent event) {
+        long currentTime = SystemClock.uptimeMillis();
+
+        return event.getActionMasked() == MotionEvent.ACTION_DOWN
+                && currentTime - mLastTouchTime <= ViewConfiguration.getDoubleTapTimeout();
+    }
+
+}
