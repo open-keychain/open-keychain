b610dd58976d615545d84aba7f4692cc9340814c
Dominik Schürmann <dominik@dominikschuermann.de>
2016-05-07 12:31:25 +0300

    Update openpgp-api-lib

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/remote/ui/RemoteBackupActivity.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/remote/ui/RemoteBackupActivity.java
@@ -18,7 +18,6 @@
 package org.sufficientlysecure.keychain.remote.ui;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
