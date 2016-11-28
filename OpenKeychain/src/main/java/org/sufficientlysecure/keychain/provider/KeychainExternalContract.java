d9784cb0ab32f8ce114057def7e6c5e09c19ead7
Vincent Breitmoser <look@my.amazin.horse>
2016-11-28 08:46:10 +0100

    externalprovider: use temp table for query
    This commit introduces a temporary table to KeychainExternal provider.
    This is used to return a result set that contains the exact query as
    identifying column, rather than the matching user id. This is helpful
    because we match case insensitively internally, while the querying
    implementation might wish to map search results against verbatim search
    strings.

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/provider/KeychainExternalContract.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/provider/KeychainExternalContract.java
@@ -38,6 +38,7 @@ public class KeychainExternalContract {
     public static class EmailStatus implements BaseColumns {
         public static final String EMAIL_ADDRESS = "email_address";
         public static final String EMAIL_STATUS = "email_status";
+        public static final String USER_ID = "user_id";
 
         public static final Uri CONTENT_URI = BASE_CONTENT_URI_EXTERNAL.buildUpon()
                 .appendPath(BASE_EMAIL_STATUS).build();
