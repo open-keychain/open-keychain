6fe0ba829ceaa3826671d6dc0e15d78ade931acc
Dominik Schürmann <dominik@dominikschuermann.de>
2016-11-29 22:46:46 +0100

    Show result in Notify after import

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/ui/ImportKeysActivity.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/ui/ImportKeysActivity.java
@@ -380,6 +380,7 @@ public class ImportKeysActivity extends BaseActivity implements ImportKeysListen
 
             // Close activities opened for importing keys and go to the list of keys
             Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra(ImportKeyResult.EXTRA_RESULT, result);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
         } else {
