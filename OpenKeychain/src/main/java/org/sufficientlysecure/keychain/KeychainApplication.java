dc913fc8ca67662aa43c77e49cf0dec43e046ad0
Vincent Breitmoser <look@my.amazin.horse>
2018-06-12 13:24:59 +0200

    remove branding of glow effect

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/KeychainApplication.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/KeychainApplication.java
@@ -87,9 +87,6 @@ public class KeychainApplication extends Application {
         }
         */
 
-        brandGlowEffect(getApplicationContext(),
-                FormattingUtils.getColorFromAttr(getApplicationContext(), R.attr.colorPrimary));
-
         // Add OpenKeychain account to Android to link contacts with keys and keyserver sync
         createAccountIfNecessary(this);
 
@@ -154,26 +151,6 @@ public class KeychainApplication extends Application {
 
         if (level >= TRIM_MEMORY_UI_HIDDEN) {
             qrCodeCache.clear();
-        }
-    }
-
-    static void brandGlowEffect(Context context, int brandColor) {
-        // no hack on Android 5
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            try {
-                // terrible hack to brand the edge overscroll glow effect
-                // https://gist.github.com/menny/7878762#file-brandgloweffect_full-java
-
-                //glow
-                int glowDrawableId = context.getResources().getIdentifier("overscroll_glow", "drawable", "android");
-                Drawable androidGlow = context.getResources().getDrawable(glowDrawableId);
-                androidGlow.setColorFilter(brandColor, PorterDuff.Mode.SRC_IN);
-                //edge
-                int edgeDrawableId = context.getResources().getIdentifier("overscroll_edge", "drawable", "android");
-                Drawable androidEdge = context.getResources().getDrawable(edgeDrawableId);
-                androidEdge.setColorFilter(brandColor, PorterDuff.Mode.SRC_IN);
-            } catch (Exception ignored) {
-            }
         }
     }
 
