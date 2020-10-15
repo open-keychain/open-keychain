89fc9a2b85d5cbeaaf849fdf6c5668a3985da6be
Austin Milne <austinwilliambrucemilne@gmail.com>
2020-10-15 00:09:00 -0400

    Formatting or Regex Comment for QRcode Vcard import

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/ui/ImportKeysProxyActivity.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/ui/ImportKeysProxyActivity.java
@@ -136,25 +136,25 @@ public class ImportKeysProxyActivity extends FragmentActivity
         }
     }
 
-/** ─── Regex Explaination ───
- *  - Full String -
- * (?<=\n)(?:KEY:OPENPGP4FPR:|KEY.*?https.*?\/pks\/lookup\?op=get&search=0x|KEY.*?https:\/\/keys.openpgp.org\/vks\/v1\/by-fingerprint\/)([a-fA-F0-9]{40})(?=\n)
- * 
- * ?:KEY:OPENPGP4FPR:                                           <- OPENPGP4FPR values
- * KEY.*?https.*?\/pks\/lookup\?op=get&search=0x                <- Majority of SKS Server pool URLs
- * KEY.*?https:\/\/keys.openpgp.org\/vks\/v1\/by-fingerprint\/  <- keys.openpgp.org URLs
- * ([a-fA-F0-9]{40})                                            <- Fingerprint
- * 
- * Matches:
- * KEY:OPENPGP4FPR:[HASH]
- * (Most) PGP Server URLs with Full Hash embedded
- * 
- * Examples:
- * KEY:OPENPGP4FPR:ABAF11C65A2970B130ABE3C479BE3E4300411886
- * KEY;PGP:https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xABAF11C65A2970B130ABE3C479BE3E4300411886
- * KEY;TYPE=PGP:https://keys.openpgp.org/vks/v1/by-fingerprint/ABAF11C65A2970B130ABE3C479BE3E4300411886
- * KEY;MEDIATYPE=application/pgp-keys:https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xabaf11c65a2970b130abe3c479be3e4300411886
- */
+    /** ─── Regex Explaination ───
+     *  - Full String -
+     * (?<=\n)(?:KEY:OPENPGP4FPR:|KEY.*?https.*?\/pks\/lookup\?op=get&search=0x|KEY.*?https:\/\/keys.openpgp.org\/vks\/v1\/by-fingerprint\/)([a-fA-F0-9]{40})(?=\n)
+     * 
+     * ?:KEY:OPENPGP4FPR:                                           <- OPENPGP4FPR values
+     * KEY.*?https.*?\/pks\/lookup\?op=get&search=0x                <- Majority of SKS Server pool URLs
+     * KEY.*?https:\/\/keys.openpgp.org\/vks\/v1\/by-fingerprint\/  <- keys.openpgp.org URLs
+     * ([a-fA-F0-9]{40})                                            <- Fingerprint
+     * 
+     * Matches:
+     * KEY:OPENPGP4FPR:[HASH]
+     * (Most) PGP Server URLs with Full Hash embedded
+     * 
+     * Examples:
+     * KEY:OPENPGP4FPR:ABAF11C65A2970B130ABE3C479BE3E4300411886
+     * KEY;PGP:https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xABAF11C65A2970B130ABE3C479BE3E4300411886
+     * KEY;TYPE=PGP:https://keys.openpgp.org/vks/v1/by-fingerprint/ABAF11C65A2970B130ABE3C479BE3E4300411886
+     * KEY;MEDIATYPE=application/pgp-keys:https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xabaf11c65a2970b130abe3c479be3e4300411886
+     */
     private static final Pattern VCARD_KEY_PATTERN = Pattern.compile("(?<=\n)(?:KEY:OPENPGP4FPR:|KEY.*?https.*?\/pks\/lookup\?op=get&search=0x|KEY.*?https:\/\/keys.openpgp.org\/vks\/v1\/by-fingerprint\/)([a-fA-F0-9]{40})(?=\n)");
 
     private void processScannedContent(String content) {
