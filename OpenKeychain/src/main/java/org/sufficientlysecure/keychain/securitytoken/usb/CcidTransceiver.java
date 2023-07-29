3404cd2f6d1786354475b7ec2bd05e37953c151d
Stuart Longland <me@vk4msl.com>
2023-07-29 14:26:35 +1000

    CcidTransceiver: Retry on time-out or zero read, up to 3 times.

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/securitytoken/usb/CcidTransceiver.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/securitytoken/usb/CcidTransceiver.java
@@ -247,15 +247,13 @@ public class CcidTransceiver {
     }
 
     private CcidDataBlock receiveDataBlockImmediate(byte expectedSequenceNumber) throws UsbTransportException {
+        int attempts = 3;
         Timber.d("Receive data block immediate seq=" + expectedSequenceNumber);
-        int readBytes = usbConnection.bulkTransfer(usbBulkIn, inputBuffer, inputBuffer.length, DEVICE_COMMUNICATE_TIMEOUT_MILLIS);
-        Timber.d("Received " + readBytes + " bytes: " + toHexString(inputBuffer));
-        if (readBytes == 0) {
-            Timber.d("Poking device again due to zero-sized read");
-            sendRaw(new byte[0], 0, 0);
+        int readBytes;
+        do {
             readBytes = usbConnection.bulkTransfer(usbBulkIn, inputBuffer, inputBuffer.length, DEVICE_COMMUNICATE_TIMEOUT_MILLIS);
-            Timber.d("(on retry) Received " + readBytes + " bytes: " + toHexString(inputBuffer));
-        }
+            Timber.d("Received " + readBytes + " bytes: " + toHexString(inputBuffer));
+        } while ((readBytes <= 0) && (attempts-- > 0));
 
         if (readBytes < CCID_HEADER_LENGTH) {
             throw new UsbTransportException("USB-CCID error - failed to receive CCID header");
